eeeb1e50f250660344d9c924fc446650db513a8b
Patrick Klæbel <Vagabonden@outlook.com>
2018-03-16 09:24:35 +0100

    Update

--- jUnit/src/BS.java
+++ jUnit/src/BS.java
@@ -1,20 +1,34 @@
+/**
+ * The BS(BookingSystem) class lets people order tickets and seats for a new movie.
+ * And display the booking and non booked seats and other useful information.
+ *
+ * @author  Pair Programming(alle)
+ * @version 1.0
+ * @since   16-03-2018
+ */
+
 import java.util.List;
 import java.util.Scanner;
 
 public class BS {
+    //Global Varaiabler.
     public static int theaterRows1 = 25, theaterSeats1 = 16;
     public static int theaterRows2 = 20, theaterSeats2 = 12;
 
     public static Database db = new Database();
     public static Theater theater = new Theater();
 
+
+    /**
+     * The seatMenu method displays the seatMenu and
+     * accepts the different options that the customer have.
+     * @param name This is the customers name
+     * @param movie  This is the movie object.
+     * @param id This is the movie ID
+     */
     public static void seatMenu(String name, Movie movie, int id){
-
-
         theater = loadTheater(id);
 
-
-        //String seats[][] = loadSeats(int showId);
         String seats[][];
         if(Integer.parseInt(movie.getTheater()) == 1) {
 
@@ -63,6 +77,11 @@ public class BS {
         }while (!(input.equals("exit")));
     }
 
+    /**
+     * The menu just display the sout's for the menu.
+     * used in seatMenu
+     * @param movie  This is the movie object.
+     */
     private static void menu(Movie movie){
         System.out.println("Booking Manager!");
         System.out.println("Movie: " + movie.getTitle());
@@ -73,6 +92,16 @@ public class BS {
         System.out.println("5. Print menu");
     }
 
+
+    /**
+     * The getSeatsMenu ask the customer for the seats they
+     * want to book and sets the theater object information
+     * @param name This is the customers name
+     * @param movie  This is the movie object.
+     * @param id This is the movie ID
+     * @param seats This is the seats 2d Array.
+     * @return  the booked seats object
+     */
     public static String[][] getSeatsMenu(String[][] seats, String name, Movie movie, int id){
         Scanner sc = new Scanner(System.in);
         int row;
@@ -93,6 +122,11 @@ public class BS {
         return seats;
     }
 
+    /**
+     * The seatShower ask for the Seats and show which
+     * seats are booked and which are not.
+     * @param seats This is the seats 2d Array.
+     */
     public static void seatShower(String[][] seats){
         System.out.print("Seats:");
         for(int o = 0; o < seats[0].length; o++)
@@ -112,6 +146,17 @@ public class BS {
         System.out.println("Type 5 to show the menu again.");
     }
 
+    /**
+     * The reservate is the menu thats calculate the seats
+     * the customer wanna book and call's the method that saves it in the database
+     * @param name This is the customers name
+     * @param id This is the movie ID
+     * @param seats This is the seats 2d Array.
+     * @param row This is the inputted row the customer wanna sit on
+     * @param seatStart This is the first seat the customer wanna buy on inputted row
+     * @param seatEnd This is the last seat the customer wanna buy on inputted row
+     * @return The reservate seats object.
+     */
     public static String[][] reservate(String[][] seats, int seatStart, int seatEnd, int row, String name, int id){
         if(checkAvailability(seats, row, seatStart, seatEnd)){
             for(int i = Math.min(seatEnd, seatStart)-1; i <= Math.max(seatEnd, seatStart)-1; i++)
@@ -123,6 +168,14 @@ public class BS {
         return seats;
     }
 
+    /**
+     * The checkAvailability method checks if the seats is available or not.
+     * @param seats This is the seats 2d Array.
+     * @param row This is the inputted row the customer wanna sit on
+     * @param seatStart This is the first seat the customer wanna buy on inputted row
+     * @param seatEnd This is the last seat the customer wanna buy on inputted row
+     * @return a boolean if those seats are available or not.
+     */
     public static boolean checkAvailability(String[][] seats, int row, int seatStart, int seatEnd){
         boolean avail = true;
         for(int i = Math.min(seatEnd, seatStart)-1; i <= Math.max(seatEnd, seatStart)-1; i++){
@@ -134,8 +187,14 @@ public class BS {
         return avail;
     }
 
+    /**
+     * The checkSeatOwner ask for the specific seat information
+     * and display who reservated those seats.
+     * @param seats This is the seats 2d Array.
+     * @return The reservated seats -1 because of array starts at 0
+     */
     public static String checkSeatOwner(String[][] seats){
-        int row = 0, seat = 0;//Potentielt dumt med 0 da den så kommer til at returne plads -1 som ikke eksisterer
+        int row = 0, seat = 0;
         Scanner sc = new Scanner(System.in);
         do{
             try{
@@ -150,6 +209,11 @@ public class BS {
         return seats[row-1][seat-1];
     }
 
+    /**
+     * The findSeats finds which seats is reservated to that customer
+     * @param seats This is the seats 2d Array.
+     * @return an int array with the seat start and end.
+     */
     public static int[] findSeats(String[][] seats){
         Scanner sc = new Scanner(System.in);
         System.out.print("Name to find seats of: ");
@@ -172,15 +236,21 @@ public class BS {
         System.out.println("Seats in the name of " + name + " was reservated to row " + seatss[2] + " with the seats " + seatss[0] + "-" + seatss[1]);
         return seatss;
     }
-
+    /**
+     * The loadTheater loads the theater specific object from the database
+     * @param showId This is the movies ID
+     * @return the specific theater object
+     */
     public static Theater loadTheater(int showId){
         List<Theater> theaters = db.LoadTheaters();
         return theaters.get(showId);
     }
-
+    /**
+     * The loadTheater saves the theater specific object to the database
+     * @param theater This is the theater object
+     */
     public static void saveTheater(Theater theater){
 
         db.SaveTheaters(theater);
-        //TODO Gem til en fil, tilknyt denne sæde liste til en film? eller omvendt?
     }
 }
